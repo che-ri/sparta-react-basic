2ef052c939ab9727a3765c064755d9c316a2c0a3
che-ri <rnjs1135@gmail.com>
2021-06-27 05:20:42 +0900

    [feat]HW3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.scss";
-import RouterPrac from "./RouterPrac/RouterPrac";
+import HW3 from "./hw/hw3/HW3";
 
 const App = () => {
-    return <RouterPrac />;
+    return <HW3 />;
 };
 
 export default App;
